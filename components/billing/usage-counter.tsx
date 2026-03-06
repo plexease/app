baab317b3e2e4ac104723ffa9ef9b0cb6c9780a0
Plexease Dev <plexease@local>
2026-03-06 13:39:59 +0000

    feat: add sidebar usage counter component

---
+++ components/billing/usage-counter.tsx
@@ -0,0 +1,18 @@
+import { FREE_MONTHLY_LIMIT } from "@/lib/constants";
+
+type Props = {
+  isPro: boolean;
+  usageCount: number;
+};
+
+export function UsageCounter({ isPro, usageCount }: Props) {
+  if (isPro) {
+    return <p className="text-xs text-gray-500">Unlimited</p>;
+  }
+
+  return (
+    <p className="text-xs text-gray-500">
+      {usageCount}/{FREE_MONTHLY_LIMIT} uses
+    </p>
+  );
+}
